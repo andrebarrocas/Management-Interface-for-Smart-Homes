8fb46bd03c6999c9cb8dc2eef065cec5ad732633
André Barrocas <andrebarrocas99@hotmail.com>
2021-03-29 20:21:09 +0100

    Erros

--- AI-Project/config/urls.py
+++ AI-Project/config/urls.py
@@ -1,7 +1,10 @@
 # config/urls.py
 from django.contrib import admin
 from django.urls import path, include # new
-
+handler404 = 'pages.views.error404'
+handler403 = 'pages.views.error403'
+handler400 = 'pages.views.error400'
+handler500 = 'pages.views.error500'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')), # new
--- AI-Project/pages/views.py
+++ AI-Project/pages/views.py
@@ -3,11 +3,22 @@ from django.http import HttpResponse
 from .platform.main import *
 
 
+
+def error404(request, exception):
+    return redirect('home')
+def error500(request):
+    return redirect('home')
+def error403(request, exception):
+    return redirect('home')
+def error400(request, exception):
+    return redirect('home')          
+
 def homePageView(request):
     light = "Off"
     t = "Lamp"
     print(getDevices()[0]["id"])
+    devices = getDevices()
     print(setSpecificDevice(5678))
     print(setSpecificDevice(1234))
     print(getSpecificDevice(5678))
-    return render(request=request, template_name='index.html', context={"light": light, "type":t})
+    return render(request=request, template_name='index.html', context={"devices": devices})
