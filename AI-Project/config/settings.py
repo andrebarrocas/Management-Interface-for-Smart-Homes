2181373e3c9ff1a256f9796616a7b3fa0ee4d711
André Barrocas <andrebarrocas99@hotmail.com>
2021-05-11 00:59:32 +0100

    Upload

--- AI-Project/config/settings.py
+++ AI-Project/config/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_read_only",
     'pages', # new
 ]
 
@@ -130,11 +129,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = 'media'
+MEDIA_URL = '/pages/platform/devices/'
