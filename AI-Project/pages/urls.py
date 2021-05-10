f8591546b5e1ec430d982c5a5cde5740745c2142
André Barrocas <andrebarrocas99@hotmail.com>
2021-05-11 00:46:42 +0100

    upload files

--- AI-Project/pages/urls.py
+++ AI-Project/pages/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import homePageView
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', homePageView, name='home'),
     path('<int:id>', homePageView, name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- AI-Project/pages/views.py
+++ AI-Project/pages/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .platform.main import *
-
-
+from django.core.files.storage import FileSystemStorage
 
 def error404(request, exception):
     return redirect('home')
@@ -16,7 +15,15 @@ def error400(request, exception):
 def homePageView(request,id=None):
     light = "Off"
     t = "Lamp"
-    devices = getDevices()
-    if not None:
-        setSpecificDevice(id)
-    return render(request=request, template_name='index.html', context={"devices": devices})
+    # devices = getDevices()
+    # if not None:
+    #     setSpecificDevice(id)
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']   
+        #print(uploaded_file.name)
+        #print(uploaded_file.size)
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return render(request=request, template_name='index.html')#, context={"devices": devices})#,'form': form})
+
+
